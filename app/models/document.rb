76cc5723d6b05d68f9ab43e5f077fa6c9fbf6a71
saroar <saroar9@gmail.com>
2017-09-29 12:24:00 +0300

    fixed clicnt side file size validation

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,5 @@
 class Document < ActiveRecord::Base
-  include SimpleCSVUploder
+  #include SimpleCSVUploder
   validate :file_size_under_one_mb, :csv_file_format, :check_duplication_filename
   before_save :upload_local
-
-
 end
